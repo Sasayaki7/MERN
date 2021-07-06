7800cc7d99e1e9012f082c06538f5ae928ebba13
Sasayaki7 <50351133+Sasayaki7@users.noreply.github.com>
2021-07-06 16:32:52 -0400

    initial destructure assignment

---
+++ Javascript/Fundamentals/destructure.js
@@ -0,0 +1,78 @@
+// const cars = ['Tesla', 'Mercedes', 'Honda']
+// const [ randomCar ] = cars
+// const [ ,otherRandomCar ] = cars
+// //Predict the output
+// console.log(randomCar)
+// console.log(otherRandomCar)
+
+// 1. 
+// 'Tesla'
+// 'Mercedes'
+
+
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// }
+// const { name: otherName } = employee;
+// //Predict the output
+// console.log(name);
+// console.log(otherName);
+
+// 2.
+// 'name'
+// 'Elon'
+
+// OK I was wrong, it would error because name isn't the key
+
+
+// const person = {
+//     name: 'Phil Smith',
+//     age: 47,
+//     height: '6 feet'
+// }
+// const password = '12345';
+// const { password: hashedPassword } = person;  
+// //Predict the output
+// console.log(password);
+// console.log(hashedPassword);
+
+// 3.
+// 12345
+// undefined.
+
+
+// const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+// const [,first] = numbers;
+// const [,,,second] = numbers;
+// const [,,,,,,,,third] = numbers;
+// //Predict the output
+// console.log(first == second);
+// console.log(first == third);
+
+// 4. 
+// false
+// true
+
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+
+
+// 5.
+// value
+// [1, 5, 1, 8, 3, 3] -> didn't print out. interesting.
+// 1
+// 5 (it should work)
