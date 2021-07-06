4dfde4274a523da009277881c138498a68333376
Sasayaki7 <50351133+Sasayaki7@users.noreply.github.com>
2021-07-06 17:51:58 -0400

    optimized prime searcher even more

--- Javascript/Fundamentals/performance.js
+++ Javascript/Fundamentals/performance.js
@@ -1,31 +1,31 @@
-// Number.prototype.isPrime = function() {
-//     let goal = Math.sqrt(this)
-//     for( let i=0; i< primes.length && i < goal; i++ ) {
-//         if( this % primes[i] === 0 ) {
-//             return false;
-//         }
-//     }
-//     primes.push(this);
-//     return true;
-// }
+Number.prototype.isPrime = function() {
+    let goal = Math.sqrt(this)
+    for( let i=0; primes[i] < goal; i++ ) {
+        if( this % primes[i] === 0 ) {
+            return false;
+        }
+    }
+    primes.push(this);
+    return true;
+}
 
-// let primes = [];
+let primes = [];
 
-// const { performance } = require('perf_hooks');
-// const start = performance.now();
-// let primeCount = 0;
-// let num = 2; // for math reasons, 1 is considered prime
-// while( primeCount < 1e6 ) {
-//     if( num.isPrime() ) {
-//         primeCount++;
-//     }
-//     num++;
-// }
-// console.log(`The 10,000th prime number is ${num-1}`);
-// console.log(`This took ${performance.now() - start} milliseconds to run`);
+const { performance } = require('perf_hooks');
+const start = performance.now();
+let primeCount = 0;
+let num = 2; // for math reasons, 1 is considered prime
+while( primeCount < 1e5 ) {
+    if( num.isPrime() ) {
+        primeCount++;
+    }
+    num++;
+}
+console.log(`The 10,000th prime number is ${num-1}`);
+console.log(`This took ${performance.now() - start} milliseconds to run`);
 
-//1e5 took 7.3s
-//1e6 took 243s
+// 1e5 took 3.1s
+// 1e6 took 75.3s
 
 
 // recursive
@@ -58,23 +58,23 @@
 //iterative: .58ms
 //Iterative >
 
-const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
-function reverseStory(str){
-    let word = '';
-    let reversedStory = '';
-    for (let i = str.length-1; i>=0;i--){
-        if (str[i] == ' '){
-            reversedStory = reversedStory+word+' '
+// const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+// function reverseStory(str){
+//     let word = '';
+//     let reversedStory = '';
+//     for (let i = str.length-1; i>=0;i--){
+//         if (str[i] == ' '){
+//             reversedStory = reversedStory+word+' '
 
-            word=''
-        }
-        else{
-            word=str[i]+word
-        }
+//             word=''
+//         }
+//         else{
+//             word=str[i]+word
+//         }
 
-    }
-    reversedStory+=word
-    return reversedStory
-}
+//     }
+//     reversedStory+=word
+//     return reversedStory
+// }
 
-console.log(reverseStory(story))
+// console.log(reverseStory(story))
