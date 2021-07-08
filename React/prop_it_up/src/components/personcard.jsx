50725a88938c627d2ea8c7bffd3714bbfb58e188
Sasayaki7 <50351133+Sasayaki7@users.noreply.github.com>
2021-07-08 15:41:13 -0400

    updated to add button

--- React/prop_it_up/src/components/personcard.jsx
+++ React/prop_it_up/src/components/personcard.jsx
@@ -2,12 +2,27 @@ import React, {Component} from 'react';
 
 
 class PersonCard extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {age: props.age}
+    }
+
+
+    increaseAge = () =>{
+        let stateX = {...this.state}
+        stateX.age++
+        this.setState(stateX)
+    }
+
+
     render(){
         return(
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.increaseAge}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         );
     }
