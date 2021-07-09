9637f5434a5d31116f0669fddd7bf9c8a09c97ea
Sasayaki7 <50351133+Sasayaki7@users.noreply.github.com>
2021-07-08 20:18:37 -0400

    added validation

--- React/hook_form/src/components/userform.jsx
+++ React/hook_form/src/components/userform.jsx
@@ -12,53 +12,41 @@ const UserForm = (props) => {
 
     
     return(
-        <>
         <form className = {styles.formOutput}>
+
             <div className={styles.inputForm}>
                 <label>First Name: </label> 
                 <input type="text" onChange={ (e) => setFirstName(e.target.value) } />
             </div>
+            {(0<firstName.length && firstName.length<2)? (<p className={styles.errorMessage}>First Name must be at least 2 characters</p>): ''}
+
             <div className={styles.inputForm}>
                 <label>Last Name: </label> 
                 <input type="text" onChange={ (e) => setLastName(e.target.value) } />
             </div>
+            {(0<lastName.length && lastName.length<2)? <p className={styles.errorMessage}>Last Name must be at least 2 characters</p>:''}
+
             <div className={styles.inputForm}>
                 <label>Email Address: </label> 
                 <input type="text" onChange={ (e) => setEmail(e.target.value) } />
             </div>
+            {(0<email.length && email.length<5)? <p className={styles.errorMessage}>Email must be at least 5 characters</p>:''}
+
+
             <div className={styles.inputForm}>
                 <label>Password: </label>
                 <input type="password" onChange={ (e) => setPassword(e.target.value) } />
             </div>
+            {(0<password.length && password.length<8)? <p className={styles.errorMessage}>Password must be at least 8 characters</p>:''}
+            {(password != confpassword && confpassword.length > 0) ? <p className={styles.errorMessage}>Paswords must match</p>:''}
+
+
             <div className={styles.inputForm}>
                 <label>Confirm Password: </label>
                 <input type="password" onChange={ (e) => setConfPassword(e.target.value) } />
             </div>
         </form>
-        <div className={styles.formDisplay}>
-            <h2>Your Form Data</h2>
-            <div className={styles.formDataDisplay}>
-                <p className={styles.label}>First Name</p>
-                <p>{firstName}</p>
-            </div>
-            <div className={styles.formDataDisplay}>
-                <p className={styles.label}>Last Name:</p>
-                <p>{lastName}</p>
-            </div>
-            <div className={styles.formDataDisplay}>
-                <p className={styles.label}>Email:</p>
-                <p>{email}</p>
-            </div>
-            <div className={styles.formDataDisplay}>
-                <p className={styles.label}>Password:</p>
-                <p>{password}</p>
-            </div>
-            <div className={styles.formDataDisplay}>
-                <p className={styles.label}>Confirm Password:</p>
-                <p>{confpassword}</p>
-            </div>
-        </div>
-        </>
+
     );
 };
     
