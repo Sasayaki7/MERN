326f14b346dbd7944402ab9a5b932b97096f0efd
Sasayaki7 <50351133+Sasayaki7@users.noreply.github.com>
2021-07-19 17:03:29 -0400

    testing Material-UI

---
+++ MERN/material-UI-test/client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LoginForm from './components/LoginForm';
+import CardComp from './components/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm/>
+      <CardComp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ MERN/material-UI-test/client/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import { Card, CardContent } from '@material-ui/core';
+
+const CardComp = ()=> {
+
+return (<Card>
+        <CardContent>
+            <h1>This is content within my card</h1>
+        </CardContent>
+    </Card>)
+}
+
+export default CardComp
